b0c64aae29ae6156b3a7b0e0f2be62e3eacb7171
Sergen Tufanoğulları <sergen@whitehorse.technology>
2020-11-17 14:41:40 +0300

    fix

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -1,1 +1,1 @@
-importScripts("https://p.dengage.com/p/push/28/b846d121-846e-74c4-6f44-be4ccbc77931/dengage_sw.js");
+importScripts("https://ptest.dengage.com/p/push/11/efce13d2-26c5-2aa7-0f9b-a144fdd735d5/dengage_sw.js");
